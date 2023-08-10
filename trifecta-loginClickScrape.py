2866434f813e208e504105378bc827853fcc433e
Ryan Lopez <rlopez4599@gmail.com>
2023-08-10 12:50:54 -0400

    - Consolidated everything I have learned into a program file. The "Trifecta" can log in, click on an element, and scrape a target element

--- trifecta-loginClickScrape.py
+++ trifecta-loginClickScrape.py
@@ -21,7 +21,7 @@ def clean_txt(text):
 def main():
     driver = get_driver()
     driver.find_element(by="id", value="id_username").send_keys("automated")
-    time.sleep(2)
+    time.sleep(1)
     driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
     driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
     time.sleep(2)
